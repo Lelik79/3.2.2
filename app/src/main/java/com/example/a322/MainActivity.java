073ad5eb92ee24ae4841cacde4f8735cee98ca27
Lelik <de_al@bk.ru>
2020-04-23 00:00:33 +0300

    Initial commit

---
+++ app/src/main/java/com/example/a322/MainActivity.java
@@ -0,0 +1,82 @@
+package com.example.a322;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView textViewInputNumbers;
+    private Button buttonChange;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        textViewInputNumbers = findViewById(R.id.textViewInputNumbers);
+
+        setCalcClickListener((TextView) findViewById(R.id.textViewButton0));
+        setCalcClickListener((TextView) findViewById(R.id.textViewButton1));
+        setCalcClickListener((TextView) findViewById(R.id.textViewButton2));
+        setCalcClickListener((TextView) findViewById(R.id.textViewButton3));
+        setCalcClickListener((TextView) findViewById(R.id.textViewButton4));
+        setCalcClickListener((TextView) findViewById(R.id.textViewButton5));
+        setCalcClickListener((TextView) findViewById(R.id.textViewButton6));
+        setCalcClickListener((TextView) findViewById(R.id.textViewButton7));
+        setCalcClickListener((TextView) findViewById(R.id.textViewButton8));
+        setCalcClickListener((TextView) findViewById(R.id.textViewButton9));
+        setCalcClickListener((TextView) findViewById(R.id.textViewButtonDot));
+
+        TextView textViewButtonC = findViewById(R.id.textViewButtonC);
+        textViewButtonC.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                textViewInputNumbers.setText("");
+
+            }
+        });
+
+
+        TextView textViewButtonE1 = findViewById(R.id.textViewButtonE1);
+        textViewButtonE1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ChangeVisibility();
+            }
+        });
+        TextView textViewButtonE2 = findViewById(R.id.textViewButtonE2);
+        textViewButtonE2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ChangeVisibility();
+            }
+        });
+
+
+    }
+
+    private void setCalcClickListener(final TextView button) {
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                textViewInputNumbers.append(button.getText());
+            }
+        });
+    }
+
+    private void ChangeVisibility() {
+        if (findViewById(R.id.relativeLayout0).getVisibility() == View.VISIBLE) {
+            findViewById(R.id.relativeLayout0).setVisibility(View.GONE);
+            findViewById(R.id.relativeLayoutEngeneering).setVisibility(View.VISIBLE);
+        } else {
+            findViewById(R.id.relativeLayout0).setVisibility(View.VISIBLE);
+            findViewById(R.id.relativeLayoutEngeneering).setVisibility(View.GONE);
+        }
+    }
+}
